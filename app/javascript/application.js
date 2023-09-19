c63aec6f92a0cef2663570f8cc36f5172733747c
enmanuelbayen <enmanuel0495@gmail.com>
2023-09-19 16:46:13 -0400

    finish setup for greetings view

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,8 +3,19 @@
 // import "./controllers"
 
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 import App from './components/App';
 
-const root = createRoot(document.getElementById('root'));
-root.render(<App/>);
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <Provider store={store}>
+        <Router>
+            <App />
+        </Router>
+    </Provider>
+);
--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Greeting from './Greeting';
 
 function App() {
   return (
-      <h1>Helo world!</h1>
-    );
+    <>
+      <Routes>
+        <Route exact path="/" element={<Greeting />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
